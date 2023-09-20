33b37b8588e1b98d8eb9d5bea5618641e48c9edb
Ariff <ariff@regovtech.com>
2023-09-20 09:56:22 +0800

    edit form body

--- src/components/FormBody.tsx
+++ src/components/FormBody.tsx
@@ -1,8 +1,4 @@
 import { useState, useEffect, useMemo } from "react";
-import Button from "react-bootstrap/Button";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
-import Row from "react-bootstrap/Row";
 
 import { StateList } from "../data/StateList";
 
@@ -69,7 +65,7 @@ function FormBody() {
         const updatedPersons = [...persons, newPerson];
         localStorage.setItem("persons", JSON.stringify(updatedPersons));
       } else {
-        window.alert("Fill all the information")
+        window.alert("Fill all the information");
       }
     } else {
       console.log("please agree first");
@@ -87,133 +83,170 @@ function FormBody() {
 
   return (
     <>
-      <Form className="mainform mb-5 mt-5" onSubmit={handleSubmit}>
-        <Form.Group className="mb-3" controlId="formGridFullName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            placeholder="Peter Parker"
-            value={person.fullname}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, fullname: e.target.value })}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formGridUserName">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            placeholder="Peter"
-            value={person.username}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, username: e.target.value })}
-          />
-        </Form.Group>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control
-              type="email"
-              placeholder="Enter email"
-              value={person.email}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, email: e.target.value })}
-            />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              type="password"
-              placeholder="Password"
-              value={person.password}
+      <div className="regForm">
+        <form onSubmit={handleSubmit}>
+          <div className="item">
+            <label htmlFor="fullname">Full Name</label>
+            <input
+              className="inputContainer"
+              type="text"
+              value={person.fullname}
               onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setPerson({ ...person, password: e.target.value })
+                setPerson({ ...person, fullname: e.target.value })
               }
+              placeholder="Peter Parker"
             />
-          </Form.Group>
-        </Row>
+          </div>
 
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            placeholder="1234 Main St"
-            value={person.address}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, address: e.target.value })}
-          />
-        </Form.Group>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridGender">
-            <Form.Label>Gender</Form.Label>
-            <Form.Select
-              defaultValue="Choose..."
-              value={person.gender}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setPerson({ ...person, gender: e.target.value })}
-            >
-              <option>Choose...</option>
-              <option>Male</option>
-              <option>Female</option>
-            </Form.Select>
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridStatus">
-            <Form.Label>Status</Form.Label>
-            <Form.Select
-              defaultValue="Choose..."
-              value={person.status}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setPerson({ ...person, status: e.target.value })}
-            >
-              <option>Choose...</option>
-              <option>Single</option>
-              <option>Married</option>
-              <option>Prefer not to say</option>
-            </Form.Select>
-          </Form.Group>
-        </Row>
-
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control
-              value={person.city}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, city: e.target.value })}
+          <div className="item">
+            <label htmlFor="fullname">Username</label>
+            <input
+              className="inputContainer"
+              type="text"
+              value={person.username}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPerson({ ...person, username: e.target.value })
+              }
+              placeholder="Peter"
             />
-          </Form.Group>
+          </div>
 
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Select
-              defaultValue="Choose..."
-              value={person.state}
-              onChange={(e: React.ChangeEvent<HTMLSelectElement>) => setPerson({ ...person, state: e.target.value })}
-            >
-              <option>Choose...</option>
-              {StateList.map((stateName, index: any) => (
-                <option key={index}>{stateName}</option>
-              ))}
-            </Form.Select>
-          </Form.Group>
+          <div className="itemhorizontal">
+            <div className="item">
+              <label htmlFor="email">Email</label>
+              <input
+                className="inputContainer"
+                type="email"
+                value={person.email}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPerson({ ...person, email: e.target.value })
+                }
+                placeholder="Email"
+              />
+            </div>
 
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPerson({ ...person, zip: parseInt(e.target.value, 10) })}
+            <div className="item">
+              <label htmlFor="password">Password</label>
+              <input
+                className="inputContainer"
+                type="password"
+                value={person.password}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPerson({ ...person, password: e.target.value })
+                }
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="item">
+            <label htmlFor="address">Address</label>
+            <input
+              className="inputContainer"
+              type="text"
+              value={person.address}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setPerson({ ...person, address: e.target.value })
+              }
+              placeholder="Address"
             />
-          </Form.Group>
-        </Row>
+          </div>
 
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check
-            type="checkbox"
-            label="I have read the terms & conditions"
-            checked={check}
-            onChange={handleCheckbox}
-          />
-        </Form.Group>
+          <div className="itemhorizontal">
+            <div className="item">
+              <label htmlFor="gender">Gender</label>
+              <select
+                name="gender"
+                className="inputContainer"
+                defaultValue={"Choose..."}
+                value={person.gender}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setPerson({ ...person, gender: e.target.value })
+                }
+              >
+                <option value="">Choose...</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+              </select>
+            </div>
 
-        <div className="text-center">
-          <Button variant="primary" type="submit" disabled={!check}>
-            Submit
-          </Button>
-        </div>
-      </Form>
+            <div className="item">
+              <label htmlFor="status">Status</label>
+              <select
+                name="status"
+                className="inputContainer"
+                defaultValue="Choose..."
+                value={person.status}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setPerson({ ...person, status: e.target.value })
+                }
+              >
+                <option value="">Choose...</option>
+                <option value="Single">Single</option>
+                <option value="Married">Married</option>
+                <option value="Prefer not to say">Prefer not to say</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="itemhorizontal">
+            <div className="item">
+              <label htmlFor="city">City</label>
+              <input
+                className="inputContainer"
+                type="text"
+                value={person.city}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPerson({ ...person, city: e.target.value })
+                }
+                placeholder="City"
+              />
+            </div>
+
+            <div className="item">
+              <label htmlFor="status">State</label>
+              <select
+                name="status"
+                className="inputContainer"
+                defaultValue="Choose..."
+                value={person.state}
+                onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+                  setPerson({ ...person, state: e.target.value })
+                }
+              >
+                <option>Choose...</option>
+                {StateList.map((stateName, index: any) => (
+                  <option key={index}>{stateName}</option>
+                ))}
+              </select>
+            </div>
+
+            <div className="item">
+              <label htmlFor="zip">Zip Code</label>
+              <input
+                className="inputContainer"
+                type="text"
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                  setPerson({ ...person, zip: parseInt(e.target.value, 10) })
+                }
+                placeholder="Zip Code"
+              />
+            </div>
+          </div>
+
+          <div className="checkForm">
+            <input type="checkbox" checked={check} onChange={handleCheckbox} />
+            <label htmlFor="vehicle1">
+              {" "}
+              I have read the terms & conditions
+            </label>
+          </div>
+
+          <div className="buttonBorder">
+            <button className="button" type="submit" disabled={!check}>Submit</button>
+          </div>
+        </form>
+      </div>
     </>
   );
 }
